0981fd396594ed85a4fa963fd0aeeab05a519c5b
José Carlos <jcolscript@gmail.com>
2020-01-30 23:49:05 -0300

    creating user update endpoiint

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -28,7 +28,39 @@ class UserController {
   }
 
   async update(req, res) {
-    return res.json({ ok: 'ok' });
+    const { email, oldPassword } = req.body;
+
+    try {
+      const user = await User.findByPk(req.userId);
+
+      if (email && email !== user.email) {
+        const isUserAccount = await User.findOne({
+          where: { email },
+        });
+
+        if (isUserAccount) {
+          return res
+            .status(400)
+            .json({ status: 400, message: 'user alredy exists.' });
+        }
+      }
+
+      if (oldPassword && !(await user.checkPassword(oldPassword))) {
+        return res
+          .status(401)
+          .json({ status: 401, message: 'password does not match' });
+      }
+
+      const { id, name, provider } = await user.update(req.body);
+
+      return res
+        .status(200)
+        .json({ status: 200, data: { id, name, email, provider } });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ status: 500, message: 'internal server error' });
+    }
   }
 }
 
