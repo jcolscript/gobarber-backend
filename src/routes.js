a017d1fbc9613f758510533689b003c49a5bf8e3
José Carlos <jcolscript@gmail.com>
2020-03-05 02:29:15 -0300

    creating avatar upload config

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,28 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+// Configs
+import multerConfig from './config/multer';
+
+// Controllers
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+// Middlewares
 import auth from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
 // User routes
 routes.post('/users', UserController.store);
 routes.put('/users', auth, UserController.update);
+
+// Avatars
+routes.post('/avatars', auth, upload.single('avatar'), (req, res) => {
+  return res.json({ message: 'ok' });
+});
 
 export default routes;
