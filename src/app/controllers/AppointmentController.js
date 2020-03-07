5843fd0099888f21f2948a1b9a959d07eb0680ab
José Carlos <jcolscript@gmail.com>
2020-03-07 01:14:11 -0300

    validating appointment dates

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 
 import Appointment from '../models/Appointments';
 import User from '../models/User';
@@ -16,8 +17,9 @@ class AppointmentController {
 
     const { provider_id, date } = req.body;
 
-    // Check if provider_id is a provider
-
+    /**
+     * Check if provider_id is a provider
+     */
     const isProvider = await User.findOne({
       where: { id: provider_id, provider: true },
     });
@@ -28,10 +30,36 @@ class AppointmentController {
         .json({ message: 'you can only create appointments with providers' });
     }
 
+    /**
+     * Check for past date
+     */
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ message: 'past date are not permitted' });
+    }
+
+    /**
+     * Check date availability
+     */
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        date: hourStart,
+        canceled_at: null,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ message: 'appointment date is not available' });
+    }
+
     const appointment = await Appointment.create({
       provider_id,
       user_id: req.userId,
-      date,
+      date: hourStart,
     });
 
     return res.status(201).json(appointment);
