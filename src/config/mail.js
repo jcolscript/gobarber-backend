c637443a3b2447747210a1f7ba0221b0b9c0352c
José Carlos <jcolscript@gmail.com>
2020-03-08 01:55:05 -0300

    creating feature fron send email of appointment canceled to provider

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '5260ed8625e04a',
+    pass: 'abb4c124f10cba',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
